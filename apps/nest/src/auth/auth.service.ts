4d41fc9a5ce838d1729089db19a3b3ff1ce05a91
Elliot Lemaire <lemaireelliot@gmail.com>
2025-02-24 19:42:47 +0100

    Setup Postman environment, check  auth routes functions OK

--- apps/nest/src/auth/auth.service.ts
+++ apps/nest/src/auth/auth.service.ts
@@ -17,16 +17,32 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signup(email: string, password: string) {
+  async signup(
+    email: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
+    console.log(email, password);
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await this.prisma.user.create({
-      data: { email, password: hashedPassword },
-    });
 
-    return this.generateToken(user.id, user.email, user.role);
+    return await this.prisma.user
+      .create({
+        data: { email, password: hashedPassword },
+      })
+      .then((data) => {
+        if (data) {
+          console.log('User created successfully');
+        }
+        return this.generateToken(data.id, data.email, data.role);
+      })
+      .catch((error) => {
+        throw error;
+      });
   }
 
-  async signin(email: string, password: string) {
+  async signin(
+    email: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
     const user = await this.prisma.user.findUnique({ where: { email } });
 
     if (!user) throw new UnauthorizedException('Invalid credentials');
@@ -39,7 +55,11 @@ export class AuthService {
     return this.generateToken(user.id, user.email, user.role);
   }
 
-  private generateToken(userId: string, email: string, role: Role) {
+  private generateToken(
+    userId: string,
+    email: string,
+    role: Role,
+  ): { access_token: string } {
     const payload: JwtPayload = { sub: userId, email, role };
     return { access_token: this.jwtService.sign(payload) };
   }
